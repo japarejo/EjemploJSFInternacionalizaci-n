ad04a89845ef78be07e0f403e5773de148fd1d08
José Antonio Parejo Maestre <japarejo@gmail.com>
2018-11-15 10:21:23 +0100

    Versión funcional de un proyecto con internacionalización.

---
+++ src/main/java/LanguageBean.java
@@ -0,0 +1,30 @@
+
+
+import java.io.Serializable;
+import java.util.Locale;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+ 
+@ManagedBean(name="language")
+@SessionScoped
+public class LanguageBean implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Locale locale = new Locale("");
+
+    public Locale getLocale() {
+        return locale;
+    }
+
+    public String getLanguage() {
+        return locale.getLanguage();
+    }
+
+    public void changeLanguage(String language) {
+        locale = new Locale(language);
+        FacesContext.getCurrentInstance().getViewRoot().setLocale(new Locale(language));
+    }
+}
